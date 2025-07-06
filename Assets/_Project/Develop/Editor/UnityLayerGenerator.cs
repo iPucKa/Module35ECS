83de7e1ac75e47e91c45f4e689bdf26cae8f65c6
iPucKa <fourtails@mail.ru>
2025-07-06 13:55:34 +0300

    Initial commit

---
+++ Assets/_Project/Develop/Editor/UnityLayerGenerator.cs
@@ -0,0 +1,52 @@
+﻿using System.IO;
+using System.Text;
+using UnityEditor;
+using UnityEngine;
+
+namespace Assets._Project.Develop.Editor
+{
+	public class UnityLayerGenerator
+	{
+		private static string OutputPath
+			=> Path.Combine(Application.dataPath, "_Project/Develop/Runtime/Utilities/UnityLayers.cs");
+
+		[InitializeOnLoadMethod]
+		[MenuItem("Tools/GenerateUnityLayers")]
+		private static void Generate()
+		{
+			StringBuilder sb = new StringBuilder();
+
+			sb.AppendLine($"public static class UnityLayers");
+			sb.AppendLine("{");
+
+			// Генерация свойств для каждого слоя
+			for (int i = 0; i < 32; i++)
+			{
+				string fullComponentName = typeof(LayerMask).FullName;
+				string componentName = typeof(LayerMask).Name;
+				string layerName = LayerMask.LayerToName(i);
+
+				if (!string.IsNullOrEmpty(layerName))
+				{
+					//sb.AppendLine($"\tpublic static readonly int Layer{layerName.Replace(" ", "")} = LayerMask.NameToLayer(\"{layerName}\");");
+					sb.AppendLine($"\tpublic static readonly int Layer{GetLayerName(layerName)} = {fullComponentName}.NameToLayer(\"{layerName}\");");
+					//sb.AppendLine($"\tpublic static readonly int LayerMask{layerName.Replace(" ", "")} = 1 << Layer{layerName.Replace(" ", "")};");
+					sb.AppendLine($"\tpublic static readonly int {componentName}{GetLayerName(layerName)} = 1 << Layer{GetLayerName(layerName)};");
+					sb.AppendLine();
+				}
+			}
+
+			sb.AppendLine("}");
+
+			File.WriteAllText(OutputPath, sb.ToString());
+
+			AssetDatabase.Refresh();
+			AssetDatabase.SaveAssets();
+		}
+
+		private static string GetLayerName(string layerName)
+		{
+			return layerName.Replace(" ", "");
+		}
+	}
+}
---
+++ Assets/_Project/Develop/Runtime/GameplayMechanics/Features/MovementFeature/CharacterControllerMovementSystem.cs
@@ -0,0 +1,51 @@
+﻿using Assets._Project.Develop.Runtime.GameplayMechanics.EntitiesCore;
+using Assets._Project.Develop.Runtime.GameplayMechanics.EntitiesCore.Systems;
+using Assets._Project.Develop.Runtime.Utilities.Reactive;
+using UnityEngine;
+
+namespace Assets._Project.Develop.Runtime.GameplayMechanics.Features.MovementFeature
+{
+	public class CharacterControllerMovementSystem : IInitializableSystem, IUpdatableSystem
+	{
+		private const float _deadZone = 0.1f;
+
+		private ReactiveVariable<Vector3> _moveDirection;
+		private ReactiveVariable<float> _moveSpeed;
+		private ReactiveVariable<float> _rotationSpeed;
+		private CharacterController _characterController;
+
+		public void OnInit(Entity entity)
+		{
+			_moveDirection = entity.MoveDirection;
+			_moveSpeed = entity.MoveSpeed;
+			_rotationSpeed = entity.RotationSpeed;
+			_characterController = entity.CharacterController;
+		}
+
+		public void OnUpdate(float deltaTime)
+		{			
+			ProcessMoveTo(deltaTime);
+
+			ProcessRotateTo(deltaTime);
+		}
+
+		private void ProcessMoveTo(float deltaTime)
+		{
+			Vector3 velocity = _moveDirection.Value.normalized * _moveSpeed.Value;
+
+			_characterController.Move(velocity * deltaTime);
+		}
+
+		private void ProcessRotateTo(float deltaTime)
+		{
+			if (_moveDirection.Value.magnitude <= _deadZone)
+				return;
+
+			Vector3 direction = _moveDirection.Value.normalized;
+			Quaternion lookRotation = Quaternion.LookRotation(direction);
+			float step = _rotationSpeed.Value * deltaTime;
+
+			_characterController.transform.rotation = Quaternion.RotateTowards(_characterController.transform.rotation, lookRotation, step);
+		}
+	}
+}
